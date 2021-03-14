08549ecb27d7cb040f4509063845a1dbe0d4f2b7
David Zhao <david@davidzhao.com>
2021-03-13 21:58:48 -0800

    added server api for RoomClient

--- go.mod
+++ go.mod
@@ -3,9 +3,10 @@ module github.com/livekit/livekit-sdk-go
 go 1.16
 
 require (
+	github.com/golang/protobuf v1.4.2
+	github.com/livekit/protocol v0.1.0
 	github.com/magefile/mage v1.11.0
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/twitchtv/twirp v7.1.1+incompatible
 	google.golang.org/protobuf v1.25.0
 )
---
+++ roomclient.go
@@ -0,0 +1,102 @@
+package sdk
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/livekit/protocol/auth"
+	"github.com/twitchtv/twirp"
+
+	livekit "github.com/livekit/livekit-sdk-go/proto"
+)
+
+type RoomServiceClient struct {
+	livekit.RoomService
+	apiKey    string
+	secretKey string
+}
+
+func NewRoomServiceClient(url string, apiKey string, secretKey string) *RoomServiceClient {
+	client := livekit.NewRoomServiceProtobufClient(url, &http.Client{})
+	return &RoomServiceClient{
+		RoomService: client,
+		apiKey:      apiKey,
+		secretKey:   secretKey,
+	}
+}
+
+func (c *RoomServiceClient) CreateRoom(ctx context.Context, req *livekit.CreateRoomRequest) (*livekit.Room, error) {
+	ctx, err := c.withAuth(ctx, auth.VideoGrant{RoomCreate: true})
+	if err != nil {
+		return nil, err
+	}
+
+	return c.RoomService.CreateRoom(ctx, req)
+}
+
+func (c *RoomServiceClient) ListRooms(ctx context.Context, req *livekit.ListRoomsRequest) (*livekit.ListRoomsResponse, error) {
+	ctx, err := c.withAuth(ctx, auth.VideoGrant{RoomList: true})
+	if err != nil {
+		return nil, err
+	}
+
+	return c.RoomService.ListRooms(ctx, req)
+}
+
+func (c *RoomServiceClient) DeleteRoom(ctx context.Context, req *livekit.DeleteRoomRequest) (*livekit.DeleteRoomResponse, error) {
+	ctx, err := c.withAuth(ctx, auth.VideoGrant{RoomCreate: true})
+	if err != nil {
+		return nil, err
+	}
+
+	return c.RoomService.DeleteRoom(ctx, req)
+}
+
+func (c *RoomServiceClient) ListParticipants(ctx context.Context, req *livekit.ListParticipantsRequest) (*livekit.ListParticipantsResponse, error) {
+	ctx, err := c.withAuth(ctx, auth.VideoGrant{RoomAdmin: true, Room: req.Room})
+	if err != nil {
+		return nil, err
+	}
+
+	return c.RoomService.ListParticipants(ctx, req)
+}
+
+func (c *RoomServiceClient) GetParticipant(ctx context.Context, req *livekit.RoomParticipantIdentity) (*livekit.ParticipantInfo, error) {
+	ctx, err := c.withAuth(ctx, auth.VideoGrant{RoomAdmin: true, Room: req.Room})
+	if err != nil {
+		return nil, err
+	}
+
+	return c.RoomService.GetParticipant(ctx, req)
+}
+
+func (c *RoomServiceClient) RemoveParticipant(ctx context.Context, req *livekit.RoomParticipantIdentity) (*livekit.RemoveParticipantResponse, error) {
+	ctx, err := c.withAuth(ctx, auth.VideoGrant{RoomAdmin: true, Room: req.Room})
+	if err != nil {
+		return nil, err
+	}
+
+	return c.RoomService.RemoveParticipant(ctx, req)
+}
+
+func (c *RoomServiceClient) MutePublishedTrack(ctx context.Context, req *livekit.MuteRoomTrackRequest) (*livekit.MuteRoomTrackResponse, error) {
+	ctx, err := c.withAuth(ctx, auth.VideoGrant{RoomAdmin: true, Room: req.Room})
+	if err != nil {
+		return nil, err
+	}
+
+	return c.RoomService.MutePublishedTrack(ctx, req)
+}
+
+func (c *RoomServiceClient) withAuth(ctx context.Context, grant auth.VideoGrant) (context.Context, error) {
+	at := auth.NewAccessToken(c.apiKey, c.secretKey)
+	at.AddGrant(&grant)
+	token, err := at.ToJWT()
+	if err != nil {
+		return nil, err
+	}
+
+	header := make(http.Header)
+	header.Set("Authorization", "Bearer "+token)
+	return twirp.WithHTTPRequestHeaders(ctx, header)
+}
