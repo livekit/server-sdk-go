43ce25e2b5cb2c27c594e78ee7e452701c044c21
David Colburn <xero73@gmail.com>
2021-06-03 18:54:20 -0500

    counting sample provider

--- sampleprovider.go
+++ sampleprovider.go
@@ -18,7 +18,7 @@ type NullSampleProvider struct {
 
 func NewNullSampleProvider(bitrate uint32) *NullSampleProvider {
 	return &NullSampleProvider{
-		SampleDuration: 1 * time.Second / 30,
+		SampleDuration: time.Second / 30,
 		BytesPerSample: bitrate / 8 / 30,
 	}
 }
@@ -26,6 +26,37 @@ func NewNullSampleProvider(bitrate uint32) *NullSampleProvider {
 func (p *NullSampleProvider) NextSample() (media.Sample, error) {
 	return media.Sample{
 		Data:     make([]byte, p.BytesPerSample),
+		Duration: p.SampleDuration,
+	}, nil
+}
+
+type CountingSampleProvider struct {
+	BytesPerSample uint32
+	SampleDuration time.Duration
+	lastSent       []byte
+}
+
+func NewCountingSampleProvider(bitrate uint32) *CountingSampleProvider {
+	bps := bitrate / 8 / 30
+	return &CountingSampleProvider{
+		SampleDuration: time.Second / 30,
+		BytesPerSample: bps,
+		lastSent:       make([]byte, bps),
+	}
+}
+
+func (p *CountingSampleProvider) NextSample() (media.Sample, error) {
+	next := make([]byte, len(p.lastSent))
+	copy(next, p.lastSent)
+	for i := len(next) - 1; i >= 0; i-- {
+		next[i] += byte(1)
+		if next[i] != byte(0) {
+			break
+		}
+	}
+	p.lastSent = next
+	return media.Sample{
+		Data:     next,
 		Duration: p.SampleDuration,
 	}, nil
 }
