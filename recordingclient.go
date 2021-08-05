d90476fa2687d38e1ce008483e8b1def7286b9e1
David Colburn <xero73@gmail.com>
2021-08-04 18:04:09 -0700

    remove extra withAuth

--- recordingclient.go
+++ recordingclient.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/livekit/protocol/auth"
-	"github.com/twitchtv/twirp"
 
 	livekit "github.com/livekit/server-sdk-go/proto"
 )
@@ -40,15 +39,4 @@ func (c *RecordingServiceClient) EndRecording(ctx context.Context, req *livekit.
 		return nil, err
 	}
 	return c.RecordingService.EndRecording(ctx, req)
-}
-
-func (c *RecordingServiceClient) withAuth(ctx context.Context, grant auth.VideoGrant) (context.Context, error) {
-	at := auth.NewAccessToken(c.apiKey, c.apiSecret)
-	at.AddGrant(&grant)
-	token, err := at.ToJWT()
-	if err != nil {
-		return nil, err
-	}
-
-	return twirp.WithHTTPRequestHeaders(ctx, newHeaderWithToken(token))
 }
