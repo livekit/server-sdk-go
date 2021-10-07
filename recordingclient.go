de49530aab86d3e7f2a41312da83b8fc8283501e
David Colburn <xero73@gmail.com>
2021-10-07 15:59:20 -0700

    update recording api

--- recordingclient.go
+++ recordingclient.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/livekit/protocol/auth"
 	livekit "github.com/livekit/protocol/proto"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 type RecordingServiceClient struct {
@@ -24,7 +25,7 @@ func NewRecordingServiceClient(url string, apiKey string, secretKey string) *Rec
 	}
 }
 
-func (c *RecordingServiceClient) StartRecording(ctx context.Context, req *livekit.StartRecordingRequest) (*livekit.RecordingResponse, error) {
+func (c *RecordingServiceClient) StartRecording(ctx context.Context, req *livekit.StartRecordingRequest) (*livekit.StartRecordingResponse, error) {
 	ctx, err := c.withAuth(ctx, auth.VideoGrant{RoomRecord: true})
 	if err != nil {
 		return nil, err
@@ -32,7 +33,23 @@ func (c *RecordingServiceClient) StartRecording(ctx context.Context, req *liveki
 	return c.RecordingService.StartRecording(ctx, req)
 }
 
-func (c *RecordingServiceClient) EndRecording(ctx context.Context, req *livekit.EndRecordingRequest) (*livekit.RecordingResponse, error) {
+func (c *RecordingServiceClient) AddOutput(ctx context.Context, req *livekit.AddOutputRequest) (*emptypb.Empty, error) {
+	ctx, err := c.withAuth(ctx, auth.VideoGrant{RoomRecord: true})
+	if err != nil {
+		return nil, err
+	}
+	return c.RecordingService.AddOutput(ctx, req)
+}
+
+func (c *RecordingServiceClient) RemoveOutput(ctx context.Context, req *livekit.RemoveOutputRequest) (*emptypb.Empty, error) {
+	ctx, err := c.withAuth(ctx, auth.VideoGrant{RoomRecord: true})
+	if err != nil {
+		return nil, err
+	}
+	return c.RecordingService.RemoveOutput(ctx, req)
+}
+
+func (c *RecordingServiceClient) EndRecording(ctx context.Context, req *livekit.EndRecordingRequest) (*emptypb.Empty, error) {
 	ctx, err := c.withAuth(ctx, auth.VideoGrant{RoomRecord: true})
 	if err != nil {
 		return nil, err
