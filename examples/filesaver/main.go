aa1ba480fd47338ebf840217bb221e36af2607de
cnderrauber <zengjiestc@gmail.com>
2022-02-15 10:18:44 +0800

    add example for save track to file (#40)
    * add example for save track to file
    
    * add comment
    
    * Update pkg/samplebuilder/samplebuilder.go
    
    Co-authored-by: David Zhao <david@davidzhao.com>
    
    * solve comments
    
    Co-authored-by: David Zhao <david@davidzhao.com>

---
+++ examples/filesaver/main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	lksdk "github.com/livekit/server-sdk-go"
+	"github.com/livekit/server-sdk-go/pkg/samplebuilder"
+	"github.com/pion/rtp/codecs"
+	"github.com/pion/webrtc/v3"
+	"github.com/pion/webrtc/v3/pkg/media"
+	"github.com/pion/webrtc/v3/pkg/media/h264writer"
+	"github.com/pion/webrtc/v3/pkg/media/ivfwriter"
+	"github.com/pion/webrtc/v3/pkg/media/oggwriter"
+)
+
+var (
+	host, apiKey, apiSecret, roomName, identity string
+)
+
+func init() {
+	flag.StringVar(&host, "host", "", "livekit server host")
+	flag.StringVar(&apiKey, "api-key", "", "livekit api key")
+	flag.StringVar(&apiSecret, "api-secret", "", "livekit api secret")
+	flag.StringVar(&roomName, "room-name", "", "room name")
+	flag.StringVar(&identity, "identity", "", "participant identity")
+}
+
+func main() {
+	flag.Parse()
+	if host == "" || apiKey == "" || apiSecret == "" || roomName == "" || identity == "" {
+		fmt.Println("invalid arguments.")
+		return
+	}
+	room, err := lksdk.ConnectToRoom(host, lksdk.ConnectInfo{
+		APIKey:              apiKey,
+		APISecret:           apiSecret,
+		RoomName:            roomName,
+		ParticipantIdentity: identity,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	room.Callback.OnTrackSubscribed = onTrackSubscribed
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT)
+
+	<-sigChan
+	room.Disconnect()
+}
+
+func onTrackSubscribed(track *webrtc.TrackRemote, publication *lksdk.RemoteTrackPublication, rp *lksdk.RemoteParticipant) {
+	fileName := fmt.Sprintf("%s-%s", rp.Identity(), track.ID())
+	fmt.Println("write track to file ", fileName)
+	NewTrackWriter(track, rp.WritePLI, fileName)
+}
+
+const (
+	maxVideoLate = 1000 // nearly 2s for fhd video
+	maxAudioLate = 200  // 4s for audio
+)
+
+type TrackWriter struct {
+	sb     *samplebuilder.SampleBuilder
+	writer media.Writer
+	track  *webrtc.TrackRemote
+}
+
+func NewTrackWriter(track *webrtc.TrackRemote, pliWriter lksdk.PLIWriter, fileName string) (*TrackWriter, error) {
+	var (
+		sb     *samplebuilder.SampleBuilder
+		writer media.Writer
+		err    error
+	)
+	switch {
+	case strings.EqualFold(track.Codec().MimeType, "video/vp8"):
+		sb = samplebuilder.New(maxVideoLate, &codecs.VP8Packet{}, track.Codec().ClockRate, samplebuilder.WithPacketDroppedHandler(func() {
+			pliWriter(track.SSRC())
+		}))
+		// ivfwriter use frame count as PTS, that might cause video played in a incorrect framerate(fast or slow)
+		writer, err = ivfwriter.New(fileName + ".ivf")
+
+	case strings.EqualFold(track.Codec().MimeType, "video/h264"):
+		sb = samplebuilder.New(maxVideoLate, &codecs.H264Packet{}, track.Codec().ClockRate, samplebuilder.WithPacketDroppedHandler(func() {
+			pliWriter(track.SSRC())
+		}))
+		writer, err = h264writer.New(fileName + ".h264")
+
+	case strings.EqualFold(track.Codec().MimeType, "audio/opus"):
+		sb = samplebuilder.New(maxAudioLate, &codecs.OpusPacket{}, track.Codec().ClockRate)
+		writer, err = oggwriter.New(fileName+".ogg", 48000, track.Codec().Channels)
+
+	default:
+		return nil, errors.New("unsupported codec type")
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	t := &TrackWriter{
+		sb:     sb,
+		writer: writer,
+		track:  track,
+	}
+	go t.start()
+	return t, nil
+}
+
+func (t *TrackWriter) start() {
+	defer t.writer.Close()
+	for {
+		pkt, _, err := t.track.ReadRTP()
+		if err != nil {
+			break
+		}
+		t.sb.Push(pkt)
+
+		for _, p := range t.sb.PopPackets() {
+			t.writer.WriteRTP(p)
+		}
+	}
+}
