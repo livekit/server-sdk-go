506bd0644e7e6465fa361feb3c985763c85b109b
David Colburn <xero73@gmail.com>
2022-02-21 21:39:38 -0800

    update protocol

--- egressclient.go
+++ egressclient.go
@@ -32,21 +32,21 @@ func (c *EgressClient) StartWebCompositeEgress(ctx context.Context, req *livekit
 	return c.Egress.StartWebCompositeEgress(ctx, req)
 }
 
-func (c *EgressClient) StartTrackCompositeEgress(ctx context.Context, req *livekit.TrackCompositeEgressRequest) (*livekit.EgressInfo, error) {
-	ctx, err := c.withAuth(ctx, auth.VideoGrant{RoomRecord: true})
-	if err != nil {
-		return nil, err
-	}
-	return c.Egress.StartTrackCompositeEgress(ctx, req)
-}
-
-func (c *EgressClient) StartTrackEgress(ctx context.Context, req *livekit.TrackEgressRequest) (*livekit.EgressInfo, error) {
-	ctx, err := c.withAuth(ctx, auth.VideoGrant{RoomRecord: true})
-	if err != nil {
-		return nil, err
-	}
-	return c.Egress.StartTrackEgress(ctx, req)
-}
+// func (c *EgressClient) StartTrackCompositeEgress(ctx context.Context, req *livekit.TrackCompositeEgressRequest) (*livekit.EgressInfo, error) {
+// 	ctx, err := c.withAuth(ctx, auth.VideoGrant{RoomRecord: true})
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return c.Egress.StartTrackCompositeEgress(ctx, req)
+// }
+//
+// func (c *EgressClient) StartTrackEgress(ctx context.Context, req *livekit.TrackEgressRequest) (*livekit.EgressInfo, error) {
+// 	ctx, err := c.withAuth(ctx, auth.VideoGrant{RoomRecord: true})
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return c.Egress.StartTrackEgress(ctx, req)
+// }
 
 func (c *EgressClient) UpdateStream(ctx context.Context, req *livekit.UpdateStreamRequest) (*livekit.EgressInfo, error) {
 	ctx, err := c.withAuth(ctx, auth.VideoGrant{RoomRecord: true})
