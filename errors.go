9f855445bbc8370109c31e52025d9c296b6c1e56
Dennis Wirya <dwirya@outlook.com>
2022-05-26 20:28:40 +1000

    refactor: Rename error type to reflect name change

--- errors.go
+++ errors.go
@@ -6,7 +6,7 @@ var (
 	ErrConnectionTimeout        = errors.New("could not connect after timeout")
 	ErrTrackPublishTimeout      = errors.New("timed out publishing track")
 	ErrCannotDetermineMime      = errors.New("cannot determine mimetype from file extension")
-	ErrUnsupportedFileType      = errors.New("MediaSampleProvider does not support this mime type")
+	ErrUnsupportedFileType      = errors.New("ReaderSampleProvider does not support this mime type")
 	ErrUnsupportedSimulcastKind = errors.New("simulcast is only supported for video")
 	ErrInvalidSimulcastTrack    = errors.New("simulcast track was not initiated correctly")
 	ErrCannotFindTrack          = errors.New("could not find the track")
