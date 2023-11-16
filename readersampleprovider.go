50e57ef6eaa1b7d8fa4e9b0ae1bb29923df53868
cnderrauber <zengjie9004@gmail.com>
2023-11-16 10:59:20 +0800

    Add sample option to create track from file/reader

--- readersampleprovider.go
+++ readersampleprovider.go
@@ -86,6 +86,12 @@ func ReaderTrackWithRTCPHandler(f func(rtcp.Packet)) func(provider *ReaderSample
 	}
 }
 
+func ReaderTrackWithSampleOptions(opts ...LocalSampleTrackOptions) func(provider *ReaderSampleProvider) {
+	return func(provider *ReaderSampleProvider) {
+		provider.trackOpts = append(provider.trackOpts, opts...)
+	}
+}
+
 // NewLocalFileTrack creates an *os.File reader for NewLocalReaderTrack
 func NewLocalFileTrack(file string, options ...ReaderSampleProviderOption) (*LocalSampleTrack, error) {
 	// File health check
