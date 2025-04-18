e7685d4a82e891e25a3057ccf8610395fa126dbe
Andrés MANELLI <amanelli@forssea-robotics.fr>
2025-04-18 12:44:58 +0200

    fix(localtrack): add missing case for h265 payloader
    Signed-off-by: Andrés MANELLI <amanelli@forssea-robotics.fr>

--- localtrack.go
+++ localtrack.go
@@ -630,6 +630,8 @@ func payloaderForCodec(codec webrtc.RTPCodecCapability) (rtp.Payloader, error) {
 	switch strings.ToLower(codec.MimeType) {
 	case strings.ToLower(webrtc.MimeTypeH264):
 		return &codecs.H264Payloader{}, nil
+	case strings.ToLower(webrtc.MimeTypeH265):
+		return &codecs.H265Payloader{}, nil
 	case strings.ToLower(webrtc.MimeTypeOpus):
 		return &codecs.OpusPayloader{}, nil
 	case strings.ToLower(webrtc.MimeTypeVP8):
