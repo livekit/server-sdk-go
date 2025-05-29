3810ef8212054c17146e27f390e8db7d6e4a81f9
Anunay Maheshwari <anunay@livekit.io>
2025-05-29 12:50:28 +0530

    chore(cleanup): split up local and remote pcm tracks

---
+++ pkg/media/constants.go
@@ -0,0 +1,11 @@
+package media
+
+import (
+	"time"
+)
+
+const (
+	DefaultOpusSampleRate     = 48000
+	DefaultOpusSampleDuration = 20 * time.Millisecond
+	defaultPCMFrameDuration   = 10 * time.Millisecond
+)
