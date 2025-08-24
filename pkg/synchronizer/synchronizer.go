002fcfaa492be5b7a74f5b88ef209adb9d38d18a
Milos Pesic <milos.pesic@livekit.io>
2025-08-24 13:10:58 +0200

    Adding an option to disable audio PTS adjustment on sender reports
    API consumers should use this option when the media processing pipeline needs stable - monotonically increasing PTS sequence - small adjustments coming from RTCP sender reports could cause gaps in the audio. Media processing pipeline could opt out of auto PTS adjustments and handle the gap by e.g modifying tempo to compensate instead

--- pkg/synchronizer/synchronizer.go
+++ pkg/synchronizer/synchronizer.go
@@ -29,8 +29,9 @@ type SynchronizerOption func(*SynchronizerConfig)
 
 // SynchronizerConfig holds configuration for the Synchronizer
 type SynchronizerConfig struct {
-	MaxTsDiff time.Duration
-	OnStarted func()
+	MaxTsDiff                  time.Duration
+	OnStarted                  func()
+	AudioPTSAdjustmentDisabled bool
 }
 
 // WithMaxTsDiff sets the maximum acceptable difference between RTP packets
@@ -45,6 +46,17 @@ func WithMaxTsDiff(maxTsDiff time.Duration) SynchronizerOption {
 func WithOnStarted(onStarted func()) SynchronizerOption {
 	return func(config *SynchronizerConfig) {
 		config.OnStarted = onStarted
+	}
+}
+
+// WithAudioPTSAdjustmentDisabled - disables auto PTS adjustments after sender reports
+// Use case: when media processing pipeline needs stable - monotonically increasing
+// PTS sequence - small adjustments coming from RTCP sender reports could cause gaps in the audio
+// Media processing pipeline could opt out of auto PTS adjustments and handle the gap
+// by e.g modifying tempo to compensate instead
+func WithAudioPTSAdjustmentDisabled() SynchronizerOption {
+	return func(config *SynchronizerConfig) {
+		config.AudioPTSAdjustmentDisabled = true
 	}
 }
 
