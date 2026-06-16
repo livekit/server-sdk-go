f2ba63f020633da53b1c1cb807bc64d46522d5ee
Denys Smirnov <dennwc@protonmail.com>
2026-06-16 13:40:19 +0200

    Add helpers for registering JSON and protobuf RPCs. (#924)

--- rpc.go
+++ rpc.go
@@ -1,8 +1,12 @@
 package lksdk
 
 import (
+	"context"
 	"fmt"
 	"time"
+
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
 
 	"github.com/livekit/protocol/livekit"
 )
@@ -77,7 +81,17 @@ type RpcInvocationData struct {
 	ResponseTimeout time.Duration
 }
 
-// Specialized error handling for RPC methods.
+// Data passed to method handler for incoming RPC invocations
+type RpcInvocationMetadata struct {
+	// The unique request ID. Will match at both sides of the call, useful for debugging or logging.
+	RequestID string
+	// The unique participant identity of the caller.
+	CallerIdentity string
+	// The maximum time the caller will wait for a response.
+	ResponseTimeout time.Duration
+}
+
+// RpcError specialized error handling for RPC methods.
 //
 // Instances of this type, when thrown in a method handler, will have their `message`
 // serialized and sent across the wire. The sender will receive an equivalent error on the other side.
@@ -89,7 +103,7 @@ type RpcError struct {
 	Data    *string
 }
 
-// Creates an error object with the given code and message, plus an optional data payload.
+// NewRpcError creates an error object with the given code and message, plus an optional data payload.
 //
 // If thrown in an RPC method handler, the error will be sent back to the caller.
 //
@@ -149,3 +163,72 @@ type rpcPendingResponseHandler struct {
 }
 
 type RpcHandlerFunc func(data RpcInvocationData) (string, error)
+type RpcHandlerCtxFunc func(ctx context.Context, data []byte) ([]byte, error)
+
+type protoMsg[T any] interface {
+	*T
+	proto.Message
+}
+
+type rpcMetadataKey struct{}
+
+func withRPCMetadata(ctx context.Context, meta *RpcInvocationMetadata) context.Context {
+	return context.WithValue(ctx, rpcMetadataKey{}, meta)
+}
+
+func RPCMetadataFromContext(ctx context.Context) *RpcInvocationMetadata {
+	meta, _ := ctx.Value(rpcMetadataKey{}).(*RpcInvocationMetadata)
+	return meta
+}
+
+var _ RoomRPCInterface = (*Room)(nil)
+
+type RoomRPCInterface interface {
+	RegisterRpcCtxMethod(method string, handler RpcHandlerCtxFunc) error
+}
+
+func RegisterRPCMethodProto[
+	Req protoMsg[ReqT],
+	Resp proto.Message,
+	ReqT any,
+](
+	r RoomRPCInterface, name string,
+	fnc func(ctx context.Context, r Req) (Resp, error),
+) error {
+	return r.RegisterRpcCtxMethod(name, func(ctx context.Context, data []byte) ([]byte, error) {
+		var req Req = new(ReqT)
+		if len(data) != 0 {
+			if err := proto.Unmarshal(data, req); err != nil {
+				return nil, err
+			}
+		}
+		resp, err := fnc(ctx, req)
+		if err != nil {
+			return nil, err
+		}
+		return proto.Marshal(resp)
+	})
+}
+
+func RegisterRPCMethodJSON[
+	Req protoMsg[ReqT],
+	Resp proto.Message,
+	ReqT any,
+](
+	r RoomRPCInterface, name string,
+	fnc func(ctx context.Context, r Req) (Resp, error),
+) error {
+	return r.RegisterRpcCtxMethod(name, func(ctx context.Context, data []byte) ([]byte, error) {
+		var req Req = new(ReqT)
+		if len(data) != 0 {
+			if err := protojson.Unmarshal(data, req); err != nil {
+				return nil, err
+			}
+		}
+		resp, err := fnc(ctx, req)
+		if err != nil {
+			return nil, err
+		}
+		return protojson.Marshal(resp)
+	})
+}
