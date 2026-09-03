b1c437b7836984cf0318ea0fe84b01cbeb30d407
Jacob Gelman <3182119+ladvoc@users.noreply.github.com>
2026-09-03 16:24:17 -0700

    Refuse to subscribe to encrypted data tracks without encryption

--- datatrack/remote.go
+++ datatrack/remote.go
@@ -419,6 +419,10 @@ func (t *RemoteTrack) Subscribe(ctx context.Context, opts ...SubscribeOption) (*
 		t.mu.Unlock()
 		return nil, ErrUnpublished
 	}
+	if t.info.UsesE2EE && m.decryptor() == nil {
+		m.mu.Unlock()
+		return nil, ErrEncryptionDisabled
+	}
 	if t.subscription == subscriptionActive {
 		stream := t.addStreamLocked(options.BufferSize)
 		t.mu.Unlock()
@@ -527,12 +531,10 @@ func (t *RemoteTrack) activate(handle trackHandle) (previous trackHandle, replac
 
 	var decryptor Decryptor
 	if t.info.UsesE2EE {
-		if decryptor = t.manager.decryptor(); decryptor == nil {
-			t.manager.params.Logger.Warnw("subscribing to an end-to-end encrypted data track without encryption enabled, frames are delivered as received", nil, "sid", t.info.SID)
-		}
+		decryptor = t.manager.decryptor()
 	}
 	t.packets = make(chan *dtp.Packet, packetBufferCount)
-	go t.runPipeline(t.packets, newRemotePipeline(decryptor, t.manager.params.Logger))
+	go t.runPipeline(t.packets, newRemotePipeline(t.info.UsesE2EE, decryptor, t.manager.params.Logger))
 	t.subHandle = handle
 	t.subscription = subscriptionActive
 	for _, waiter := range t.waiters {
@@ -717,13 +719,14 @@ func (s *Stream) close() {
 
 // remotePipeline turns a subscription's packets back into frames. It is owned by one goroutine.
 type remotePipeline struct {
+	encrypted    bool
 	decryptor    Decryptor
 	log          logger.Logger
 	depacketizer *depacketizer
 }
 
-func newRemotePipeline(decryptor Decryptor, log logger.Logger) *remotePipeline {
-	return &remotePipeline{decryptor: decryptor, log: log, depacketizer: newDepacketizer()}
+func newRemotePipeline(encrypted bool, decryptor Decryptor, log logger.Logger) *remotePipeline {
+	return &remotePipeline{encrypted: encrypted, decryptor: decryptor, log: log, depacketizer: newDepacketizer()}
 }
 
 // processPacket reports whether the packet completed a frame.
@@ -738,8 +741,12 @@ func (p *remotePipeline) processPacket(packet *dtp.Packet, maxPartialFrames int)
 	}
 
 	frame := Frame{Payload: result.frame.payload, UserTimestamp: result.frame.extensions.UserTimestamp}
-	if p.decryptor == nil {
+	if !p.encrypted {
 		return frame, true
+	}
+	if p.decryptor == nil {
+		p.log.Errorw("dropping encrypted data track frame, data encryption is not enabled", nil)
+		return Frame{}, false
 	}
 	e2ee := result.frame.extensions.E2EE
 	if e2ee == nil {
